d04773e3880ba4686299663cf30aae1a757b4e4f
YouQian <1048474651@qq.com>
2024-10-24 11:51:55 +0800

    2024-10-24 项目创建&继承vant-ui

---
+++ pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				show: false
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+</style>
